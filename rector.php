7ef61976632a9113407a58b026e05e32776d212a
Stanislav Janů <s@janu.software>
2022-05-09 11:40:11 +0200

    Rector config upgrade

--- rector.php
+++ rector.php
@@ -3,38 +3,38 @@
 declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\ClassMethod\DateTimeToDateTimeInterfaceRector;
+use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\SetList;
 use Rector\Nette\Set\NetteSetList;
 use Rector\TypeDeclaration\Rector\FunctionLike\ReturnTypeDeclarationRector;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (RectorConfig $rectorConfig): void {
 	// get parameters
-	$parameters = $containerConfigurator->parameters();
+	$parameters = $rectorConfig->parameters();
 
-	$parameters->set(Option::PATHS, [
+	$rectorConfig->paths([
 		__DIR__ . '/src',
 	]);
 
-	$parameters->set(Option::AUTO_IMPORT_NAMES, true);
+	$rectorConfig->importNames();
 	$parameters->set(Option::CACHE_DIR, __DIR__ . '/temp/rector');
 
 	// Define what rule sets will be applied
-	$containerConfigurator->import(SetList::PHP_80);
-	$containerConfigurator->import(SetList::CODE_QUALITY);
-	$containerConfigurator->import(SetList::NAMING);
-	$containerConfigurator->import(SetList::PSR_4);
-	$containerConfigurator->import(SetList::TYPE_DECLARATION_STRICT);
-	$containerConfigurator->import(NetteSetList::NETTE_CODE_QUALITY);
+	$rectorConfig->import(SetList::PHP_80);
+	$rectorConfig->import(SetList::CODE_QUALITY);
+	$rectorConfig->import(SetList::NAMING);
+	$rectorConfig->import(SetList::PSR_4);
+	$rectorConfig->import(SetList::TYPE_DECLARATION_STRICT);
+	$rectorConfig->import(NetteSetList::NETTE_CODE_QUALITY);
 
-	$parameters->set(Option::SKIP, [
+	$rectorConfig->skip([
 		DateTimeToDateTimeInterfaceRector::class
 	]);
-	$parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
+	$rectorConfig->phpVersion(PhpVersion::PHP_80);
 
-	$services = $containerConfigurator->services();
+	$services = $rectorConfig->services();
 	$services->set(ReturnTypeDeclarationRector::class);
 };
